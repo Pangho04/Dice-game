18d2b576d14964eeb5073646b2683e11a514008a
Pangho04 <dlckdgh741@naver.com>
2024-05-19 03:36:55 +0900

    Feat: 주요 기능 구현
    - 각 Player card 컴포넌트
    - 던지기, 리셋 버튼
    - 연관된 각종 기능들

--- src/Menu.js
+++ src/Menu.js
@@ -1,8 +1,48 @@
-export default function Menu() {
+export default function Menu({
+  playerDice,
+  getDice,
+  handlePlayer1Dice,
+  player,
+  handlePlayer,
+  hadleplayerdice,
+  handlePlayer1TurnScore,
+  handlePlayer1TotalScore,
+  handlePlayer2TurnScore,
+  handlePlayer2TotalScore,
+  handlePlayerHistory,
+}) {
+  function turnPlayer () {
+    if (player === 'red') {
+      handlePlayer('yellow')
+    } else if (player === 'yellow') {
+      handlePlayer('red')
+    }
+  }
+
+  /**리셋 버튼 */
+  function resetTurn () {
+    handlePlayer1Dice(null);
+    handlePlayer('yellow');
+    handlePlayer1TurnScore([]);
+    handlePlayer1TotalScore(0);
+    handlePlayer2TurnScore([]);
+    handlePlayer2TotalScore(0);
+    getDice();
+
+  }
+
+/**함수묶음(주사위 던지기, 사용자 변경, 눈 기록, 총점수) */
+  function throwBtn() {
+    getDice();
+    turnPlayer();
+    handlePlayerHistory();
+    hadleplayerdice();
+  }
 
     return (
         <div className="menus">
-
+            <button className='throw-Btn' onClick={throwBtn}>던지기</button>
+            <button className='reset-Btn' onClick={resetTurn}>처음부터</button>
         </div>
     )
 }
