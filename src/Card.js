b1167315084d8480e74abc1d7c7299c2e821fe8b
Pangho04 <dlckdgh741@naver.com>
2024-05-19 20:35:42 +0900

    Design: CSS 수정

--- src/Card.js
+++ src/Card.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Card({
   name,
@@ -7,6 +7,7 @@ export default function Card({
   playerTotalScore,
 }) {
   const playerHistory = playerTurnScores.join(', ');
+  const [animate, setAnimate] = useState(false);
 
   if (diceImg === 'yellow0') {
     diceImg = 'yellow01';
@@ -14,10 +15,20 @@ export default function Card({
     diceImg = 'red01';
   }
 
+  useEffect(() => {
+    setAnimate(true);
+
+    setTimeout(() => {
+      setAnimate(false);
+    }, 300);
+  },[playerTurnScores]);
+
+  console.log(animate)
+
   return(
     <div className="card">
       <h4>{name}</h4>
-      <img className='dices' src={require(`./image/dice-${diceImg}.png`)} alt={diceImg}/>
+      <img className={`dices ${animate ? 'animate' : ''}`}  src={require(`./image/dice-${diceImg}.png`)} alt={diceImg}/>
       <p className='score'>총점</p>
       <p>{playerTotalScore}</p>
       <p className='history'>기록</p>
